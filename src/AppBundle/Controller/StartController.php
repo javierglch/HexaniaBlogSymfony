6a58fe6270531f9b2186573cb76bbf0bffb92721
Javier <javierglch@hotmail.com>
2017-05-05 14:32:02 +0200

    Añadida la funcionalidad multi idioma y continuando con el layout

--- src/AppBundle/Controller/StartController.php
+++ src/AppBundle/Controller/StartController.php
@@ -6,50 +6,60 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
+use Symfony\Component\HttpFoundation\Session\Session;
 /**
  * @Route("/")
  */
 class StartController extends Controller {
 
-    //
-    //@Route("/{id}", requirements={"id" = "\d+"}, defaults={"id" = 1}, name="blog_home")
-    //Route("/{id}")
-    //@Method({"GET", "POST"})
-    //@ParamConverter("post", class="SensioBlogBundle:Post")
-    //@ParamConverter(converter="name")
-    //@ParamConverter("post", class="SensioBlogBundle:Post", options={"id" = "post_id"})	
-    //@ParamConverter("post", class="SensioBlogBundle:Post", options={"entity_manager" = "foo"})
-    //@ParamConverter("comment", class="SensioBlogBundle:Comment", options={"id" = "comment_id"})
-    //@ParamConverter("post", options={"mapping": {"date": "date", "slug": "slug"}})
-    //@ParamConverter("comment", options={"mapping": {"comment_slug": "slug"}})
-    //@Route("/blog/{date}/{slug}")
-    //@ParamConverter("post", options={"exclude": {"date"}})
-    //@ParamConverter("post", class="SensioBlogBundle:Post", options={"repository_method" = "findWithJoins"})
-    //@ParamConverter("user", class="AcmeBlogBundle:User", options={
-    //   "repository_method" = "findByFullName",
-    //   "mapping": {"first_name": "firstName", "last_name": "lastName"},
-    //   "map_method_signature" = true
-    // })
-    //@Security("has_role('ROLE_ADMIN')")
-    //@Security("is_granted('POST_SHOW', post)")
-    //@Security("is_granted('POST_SHOW', post) and has_role('ROLE_ADMIN')")
-    //
+	//
+	//@Route("/{id}", requirements={"id" = "\d+"}, defaults={"id" = 1}, name="blog_home")
+	//Route("/{id}")
+	//@Method({"GET", "POST"})
+	//@ParamConverter("post", class="SensioBlogBundle:Post")
+	//@ParamConverter(converter="name")
+	//@ParamConverter("post", class="SensioBlogBundle:Post", options={"id" = "post_id"})	
+	//@ParamConverter("post", class="SensioBlogBundle:Post", options={"entity_manager" = "foo"})
+	//@ParamConverter("comment", class="SensioBlogBundle:Comment", options={"id" = "comment_id"})
+	//@ParamConverter("post", options={"mapping": {"date": "date", "slug": "slug"}})
+	//@ParamConverter("comment", options={"mapping": {"comment_slug": "slug"}})
+	//@Route("/blog/{date}/{slug}")
+	//@ParamConverter("post", options={"exclude": {"date"}})
+	//@ParamConverter("post", class="SensioBlogBundle:Post", options={"repository_method" = "findWithJoins"})
+	//@ParamConverter("user", class="AcmeBlogBundle:User", options={
+	//   "repository_method" = "findByFullName",
+	//   "mapping": {"first_name": "firstName", "last_name": "lastName"},
+	//   "map_method_signature" = true
+	// })
+	//@Security("has_role('ROLE_ADMIN')")
+	//@Security("is_granted('POST_SHOW', post)")
+	//@Security("is_granted('POST_SHOW', post) and has_role('ROLE_ADMIN')")
+	//
 	public function exampleAction(Request $request, $id) {
-        return $this->render('index.html.twig', ['var' => 'hola mundo!']);
-    }
+		return $this->render('index.html.twig', ['var' => 'hola mundo!']);
+	}
 
-    /**
-     * @Route("/", name="start_index")
-     */
-    public function indexAction(Request $request) {
+	/**
+	 * @Route("/", name="start_index")
+	 */
+	public function indexAction(Request $request) {
 
-        $sql = "select * from  articles";
-        $em = $this->getDoctrine()->getManager();
-        $stmt = $em->getConnection()->prepare($sql);
-        $stmt->execute();
-        $rs = $stmt->fetchAll();
-        return $this->render('index.html.twig', ['var' => 'hola mundo!']);
-    }
+//        $sql = "select * from  articles";
+//        $em = $this->getDoctrine()->getManager();
+//        $stmt = $em->getConnection()->prepare($sql);
+//        $stmt->execute();
+//        $rs = $stmt->fetchAll();
+		return $this->render('index.html.twig', ['var' => 'hola mundo!']);
+	}
+
+	/**
+	 * @Route("/lang/{locale}", defaults={"locale": "en"}, requirements={"locale" = "en|es_ES"}, name="start_set_locale")
+	 * @param Request $request
+	 * @param string $locale
+	 */
+	public function setLocaleAction(Request $request, $locale) {
+		$request->getSession()->set('_locale', $locale);
+		return $this->redirectToRoute('start_index');
+	}
 
 }
